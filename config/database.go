cf2b93e89612d610c45c9665fc810c41e4624452
Truong26 <truongnn@avadagroup.com>
2025-04-05 15:46:19 +0700

    update conncet database if database not found

--- config/database.go
+++ config/database.go
@@ -1,32 +1,69 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 // DB is global for database access
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// create string connect
-	//dsn := fmt.Sprintf(
-	//	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-	//	os.Getenv("DB_HOST"),
-	//	os.Getenv("DB_USER"),
-	//	os.Getenv("DB_PASSWORD"),
-	//	os.Getenv("DB_NAME"),
-	//	os.Getenv("DB_PORT"),
-	//)
+	createDatabaseIfNotExists()
 
-	// connect database using GORM with
-	//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	//if err != nil {
-	//	log.Fatalf("Cannot database: %v", err)
-	//}
+	//create string connect
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
-	// save database global
-	//DB = db
+	//connect database using GORM with
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Cannot database: %v", err)
+	}
 
-	fmt.Println("✅ Connect PostgreSQL success!")
+	//save database global
+	DB = db
+}
+
+func createDatabaseIfNotExists() {
+	// Connect Postgres
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to PostgreSQL server: %v", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	dbName := os.Getenv("DB_NAME")
+
+	// Create database if not found
+	_, err = db.Exec("CREATE DATABASE " + dbName)
+	if err != nil {
+		log.Printf("Warning (ignore if already exists): %v", err)
+	} else {
+		log.Println("Database created:", dbName)
+	}
 }
