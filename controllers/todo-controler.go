98c2c47c313e719f0ffee7d9b536a8f716c8a778
Truong26 <truongnn@avadagroup.com>
2025-04-05 23:17:55 +0700

    udpate func handle create todo with validate title

--- controllers/todo-controler.go
+++ controllers/todo-controler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"example.com/crud-go/models"
 	"example.com/crud-go/repository"
+	"fmt"
 	"net/http"
 )
 
@@ -12,20 +13,26 @@ var todoRepo = &repository.TodoRepository{}
 func CreateTodoList(w http.ResponseWriter, r *http.Request) {
 	var todo models.Todo
 
+	// Decode JSON to struct
 	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
 		http.Error(w, "Invalid input", http.StatusBadRequest)
 		return
 	}
 
-	err := todoRepo.Create(&todo)
+	if err := todo.Validate(); err != nil {
+		http.Error(w, fmt.Sprintf("Invalid data: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
 
-	if err != nil {
+	isErr := todoRepo.Create(&todo)
+
+	if isErr != nil {
 		http.Error(w, "error create tode", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(todo)
+	err := json.NewEncoder(w).Encode(todo)
 	if err != nil {
 		return
 	}
--- models/todo.go
+++ models/todo.go
@@ -1,11 +1,41 @@
 package models
 
-import "time"
+import (
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+	"time"
+)
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
 
 type Todo struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	Title     string    `gorm:"index" json:"title"`
+	Title     string    `gorm:"index unique;not null" json:"title" validate:"required,min=10" json:"title"`
 	Completed bool      `json:"completed"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (todo *Todo) Validate() error {
+	return validate.Struct(todo)
+}
+
+func (todo *Todo) Save(db *gorm.DB) error {
+	if err := db.Create(&todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindTodoByTitle(db *gorm.DB, title string) (*Todo, error) {
+	var todo Todo
+	err := db.Where("title = ?", title).First(&todo).Error
+	if err != nil {
+		return nil, err
+	}
+	return &todo, nil
 }
